702092a6946cdfab537beab14f0a301cc2b90b16
jennie.shao <m65pjj@gmail.com>
2023-03-20 22:53:38 +0800

    test: widget lifecycle

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,8 +8,17 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('init func ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build func ran');
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -17,7 +26,14 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose a location'),
         elevation: 0,
       ),
-      body: Text('ChooseLocation'),
+      body: ElevatedButton(
+        child: Text('counter: $counter'),
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+      ),
     );
   }
 }
