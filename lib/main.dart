36256f35e78adb8ce4725fdb5ac5b033f8688fbc
jennie.shao <m65pjj@gmail.com>
2023-03-20 22:39:01 +0800

    feat: add navigation

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // home: Home(), // 與 '/'(Loading) conflict，改用 initialRoute
+    initialRoute: '/home', // overwrite 初始畫面
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => ChooseLocation()
+    },
+  ));
+}
