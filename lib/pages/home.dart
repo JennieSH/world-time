2c530088d259f473b96d54aaa3e589f4712137f9
jennie.shao <m65pjj@gmail.com>
2023-03-21 15:15:05 +0800

    feat: format date

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,21 +14,49 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // get route data
     data = ModalRoute.of(context)?.settings.arguments as Map;
-    print(data);
 
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          TextButton.icon(
-            onPressed: () {
-              // 前往 /location 頁面
-              Navigator.pushNamed(context, '/location');
-            },
-            icon: Icon(Icons.edit_location),
-            label: Text('edit location'),
-          )
-        ],
+          child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+        child: Column(
+          children: [
+            TextButton.icon(
+              onPressed: () {
+                // 前往 /location 頁面
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: Icon(
+                Icons.edit_location,
+                color: Colors.grey[900],
+              ),
+              label: Text(
+                'Edit location',
+                style: TextStyle(color: Colors.grey[900]),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  data['location'],
+                  style: TextStyle(
+                    fontSize: 28.0,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              data['time'],
+              style: TextStyle(
+                fontSize: 66.0,
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
